024a7ffa72e312154eadacb78e8711db75a7a6e4
kiranmv9 <kiranmv53471@gmail.com>
2023-06-03 12:00:52 +0530

    added getter setter

--- src/main/java/com/ty/Person.java
+++ src/main/java/com/ty/Person.java
@@ -19,5 +19,45 @@ public class Person {
 	@OneToOne
 	private Pancard pancard;
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public long getPhone() {
+		return phone;
+	}
+
+	public void setPhone(long phone) {
+		this.phone = phone;
+	}
+
+	public Pancard getPancard() {
+		return pancard;
+	}
+
+	public void setPancard(Pancard pancard) {
+		this.pancard = pancard;
+	}
+
 	
 }
